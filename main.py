297b9966ffce7e29a43030d6665db9fe5a248b8e
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-01 06:37:37 +0200

    Update text reading @ main

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from textblob import TextBlob
 from newspaper import Article
+import nltk
 
+nltk.download('punkt')
+
+url = 'https://en.wikipedia.org/wiki/Cartier_(jeweler)'
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+text = article.text
+print(text)
