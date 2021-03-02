10dc35f7ba71e758ab5b1ad3c4e2f639a9eaa45a
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-02 04:25:38 +0200

    Update own_text_analysis.py

--- own_text_analysis.py
+++ own_text_analysis.py
@@ -1,6 +1,7 @@
 from textblob import TextBlob
 
-
+with open('Machine_Learning-Sentiment_Text_Analysis\own_text.txt', 'r') as f:
+    text = f.read()
 
 blob = TextBlob(text)
 sentiment = blob.sentiment.polarity  # -1 to 1
