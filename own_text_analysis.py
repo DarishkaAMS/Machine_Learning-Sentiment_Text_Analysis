7089571c2c7348131fd376d0435e1a8c2046567d
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-03-02 04:29:26 +0200

    Add own_negative_text & own_positive_text analysis

--- own_text_analysis.py
+++ own_text_analysis.py
@@ -1,8 +1,16 @@
 from textblob import TextBlob
 
-with open('Machine_Learning-Sentiment_Text_Analysis\own_text.txt', 'r') as f:
+with open('Machine_Learning-Sentiment_Text_Analysis\own_positive_text.txt', 'r') as f:
     text = f.read()
 
 blob = TextBlob(text)
 sentiment = blob.sentiment.polarity  # -1 to 1
-print(sentiment)
+print("Positive sentiment", sentiment)
+
+
+with open('Machine_Learning-Sentiment_Text_Analysis\own_negative_text.txt', 'r') as f:
+    text = f.read()
+
+blob = TextBlob(text)
+sentiment = blob.sentiment.polarity  # -1 to 1
+print("Negative sentiment", sentiment)
